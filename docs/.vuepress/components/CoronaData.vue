30ff8e26a6df4a85a05197fe40487e6a9d835158
Martin Lux <martin.lux@aoe.com>
2020-03-21 19:44:22 +0100

    Styling and add new url for api

--- docs/.vuepress/components/CoronaData.vue
+++ docs/.vuepress/components/CoronaData.vue
@@ -1,10 +1,34 @@
 <template>
   <div>
-    <div
-      v-for="(data, index) in coronaData"
-      :key="data.index"
+    <button
+      style="padding: 5px"
+      v-on:click="fetchCoronaData"
     >
-    {{ data.country }}
+        Click here to get COVID19 data
+    </button>
+    <p v-if="isFetching">fetching data... </P>
+    <div v-else>
+      <table v-if="coronaData.length > 0">
+        <tr>
+          <th>country</th>
+          <th>active</th>
+          <th>recovered</th>
+          <th>deaths</th>
+          <th>source</th>
+          <th>rating</th>
+        </tr>
+        <tr
+          v-for="(data, index) in coronaData"
+          :key="data.index"
+        >
+          <td>{{ data.country }}</td>
+          <td>{{ data.active }}</td>
+          <td>{{ data.recovered }}</td>
+          <td>{{ data.deaths }}</td>
+          <td>{{ data.url }}</td>
+          <td>{{ data.rating }}</td>
+        </tr>
+      </table>
     </div>
   </div>
 </template>
@@ -16,18 +40,25 @@ export default {
   name:"corona-data",
   data() {
     return {
-      coronaData: []
+      coronaData: [],
+      fetch: axios,
+      isFetching: false
     };
   },
-  beforeMount() {
-    axios
-      .get("https://corona.ndo.dev/")
-      .then(response => {
-        this.$data.coronaData = response.data
-      })
-      .catch(error => {
-        console.log(error)
-      })
+  methods: {
+    fetchCoronaData() {
+      this.isFetching = true
+      return this.fetch
+        .get("https://corona.ndo.dev/api/daily")
+        .then(response => {
+          this.$data.coronaData = response.data
+          this.isFetching = false
+        })
+        .catch(error => {
+          this.isFetching = false
+          console.log(error)
+        })
+    }
   }
 };
 </script>
