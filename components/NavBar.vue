93f43434b9276af4f7e6e47c8ee48a89cfa7143a
Martin Lux <martin.lux@aoe.com>
2020-03-21 12:51:16 +0100

    Clean up

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -1,18 +1,7 @@
 <template>
   <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-6">
     <div class="flex items-center flex-shrink-0 text-white mr-6">
-      <svg
-        class="fill-current h-8 w-8 mr-2"
-        width="54"
-        height="54"
-        viewBox="0 0 54 54"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"
-        />
-      </svg>
-      <span class="font-semibold text-xl tracking-tight">Tailwind CSS</span>
+      <span class="font-semibold text-xl tracking-tight">CoronaAPI</span>
     </div>
     <div class="block lg:hidden">
       <button
@@ -31,30 +20,18 @@
     <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
       <div class="text-sm lg:flex-grow">
         <a
-          href="#responsive-header"
+          href="https://corona.ndo.dev/api-docs/swagger-ui/#/default/get_"
           class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
         >
-          Docs
-        </a>
-        <a
-          href="#responsive-header"
-          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-        >
-          Examples
-        </a>
-        <a
-          href="#responsive-header"
-          class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-        >
-          Blog
+          API
         </a>
       </div>
       <div>
         <a
           class="inline-block font-semibold text-xl px-2 rounded py-2 leading-none text-white hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
-          href="#"
+          href="https://github.com/CoronaAPI"
         >
-          <fa :icon="faGithub" />
+          <fa size="lg" :icon="faGithub" />
         </a>
       </div>
     </div>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <NavBar />
-    <nuxt />
+    <header>
+      <NavBar />
+    </header>
+    <main class="container">
+      <nuxt />
+    </main>
   </div>
 </template>
 
@@ -16,6 +20,15 @@ export default {
 </script>
 
 <style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
     Roboto, 'Helvetica Neue', Arial, sans-serif;
