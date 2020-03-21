22c4021d4eab5afcbb3e4eef5980addce4d0fa8f
Martin Lux <martin.lux@aoe.com>
2020-03-21 15:49:56 +0100

    Add config

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: 'CoronaAPI',
+  description: 'The CoronaAPI that scales :-) Here you can find all information to consume or contribute data of the COVID-19 virus.',
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      { text: 'Guide', link: '/guide/' },
+      { text: 'API', link: 'https://corona.ndo.dev/api-docs/swagger-ui' }
+    ]
+  }
+}
