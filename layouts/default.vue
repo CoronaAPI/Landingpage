82e613a5bad0f979510a1988603a6e8f501c8a5a
Martin Lux <martin.lux@aoe.com>
2020-03-21 11:50:34 +0100

    Add Github icon

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,19 @@
 <template>
   <div>
+    <NavBar />
     <nuxt />
   </div>
 </template>
+
+<script>
+import NavBar from '~/components/NavBar.vue'
+
+export default {
+  components: {
+    NavBar
+  }
+}
+</script>
 
 <style>
 html {
