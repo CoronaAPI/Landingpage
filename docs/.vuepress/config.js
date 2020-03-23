c7529d81f2b422115e7199c4ea56f7171dfb96fe
ndo@ndo3 <yo@ni.co.de>
2020-03-23 12:30:45 +0100

    add: favicon / manifest.json

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,6 +2,75 @@ module.exports = {
   title: "CoronaAPI",
   description:
     "The CoronaAPI that scales :-) Here you can find all information to consume or contribute data of the COVID-19 virus.",
+  head: [
+    [
+      "link",
+      {
+        rel: "apple-touch-icon",
+        sizes: "180x180",
+        href: "/favicon/apple-touch-icon.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "32x32",
+        href: "/favicon/favicon-32x32.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "192x192",
+        href: "/favicon/android-chrome-192x192.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "512x512",
+        href: "/favicon/android-chrome-512x512.png"
+      }
+    ],
+    [
+      "link",
+      {
+        rel: "icon",
+        type: "image/png",
+        sizes: "16x16",
+        href: "/favicon/favicon-16x16.png"
+      }
+    ],
+    ["link", { rel: "manifest", href: "/manifest.json" }],
+    [
+      "link",
+      {
+        rel: "mask-icon",
+        href: "/favicon/safari-pinned-tab.svg",
+        color: "#3a0839"
+      }
+    ],
+    ["link", { rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    [
+      "link",
+      { rel: "shortcut icon", type: "image/x-icon", href: "/favicon.ico" }
+    ],
+    ["meta", { name: "msapplication-TileColor", content: "#3a0839" }],
+    [
+      "meta",
+      {
+        name: "msapplication-config",
+        content: "/favicon/browserconfig.xml"
+      }
+    ],
+    ["meta", { name: "theme-color", content: "#ffffff" }]
+  ],
   themeConfig: {
     nav: [
       { text: "Home", link: "/" },
