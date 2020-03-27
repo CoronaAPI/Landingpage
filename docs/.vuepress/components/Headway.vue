7d35b91ff8bbaca2313047fd2418cb85a1f357b9
Martin Lux <martin.lux@aoe.com>
2020-03-27 18:00:16 +0100

    Fix - headway

--- docs/.vuepress/components/Headway.vue
+++ docs/.vuepress/components/Headway.vue
@@ -1,7 +1,17 @@
+<template>
+  <div v-click-outside="hide" class="changelog" />
+</template>
+
 <script>
-var HW_config = {
-  selector: ".links",
-  account: "J5ggqx"
-};
-Headway.init(HW_config);
+
+export default {
+  name: "headway",
+  mounted() {
+    const HW_config = {
+      selector: ".links",
+      account: "J5ggqx"
+    };
+    Headway.init(HW_config);
+  }
+}
 </script>
